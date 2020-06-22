2cbb97617380288ba17d62f56acbff7d469416f5
Daniel Jenkala <djenkala@yahoo.com>
2020-06-22 00:13:12 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.valid?
-      @post.update(post_params)
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
+    @post.update(post_params)
+      if @post.valid?
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
   end
 
   private
